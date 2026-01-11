7dbda25ebde43764f8117ae84557bf0f306115fa
EnderRomantice <enderromantic@gmail.com>
2026-01-12 00:52:13 +0800

    feat: init blog

---
+++ src/config/contact.ts
@@ -0,0 +1,6 @@
+export const contact = {
+  name: "Ender",
+  x: "https://x.com/EnderRoman18112",
+  mastodon: "https://mastodon.social/@enderromantic",
+  github: "https://github.com/EnderRomantice"
+}
---
+++ src/config/index_img.ts
@@ -0,0 +1,14 @@
+export const index_img = [
+  {
+    title: "Dragon Water Sky",
+    img: "src/assets/Dragon Water Sky.jpg",
+    description: "cold and warm sky",
+    date: "forget"
+  },
+  {
+    title: "TaiWanese Mixed Noodles",
+    img: "src/assets/TaiwaneseMixedNoodles.jpg",
+    description: "little spicy",
+    date: "forget"
+  }
+];
