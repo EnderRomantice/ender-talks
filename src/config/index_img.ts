36cd89d3729b38f790887ae64018cabc81cd9607
EnderRomantice <enderromantic@gmail.com>
2026-01-12 03:26:50 +0800

    fix: image url issue

--- src/config/index_img.ts
+++ src/config/index_img.ts
@@ -1,14 +1,59 @@
+import DragonWaterSky from "../assets/Dragon Water Sky.jpg"
+import TaiwaneseMixedNoodles from "../assets/TaiwaneseMixedNoodles.jpg"
+import GrassWater from "../assets/Grass Water.jpg"
+import ICPC from "../assets/ICPC Coffee.jpg"
+import WuHouHouse from "../assets/Wu Hou House.jpg"
+import SchoolSky from "../assets/School Sky.jpg"
+import GuangHanSky from "../assets/Guang Han Sky.jpg"
+
 export const index_img = [
   {
     title: "Dragon Water Sky",
-    img: "src/assets/Dragon Water Sky.jpg",
-    description: "cold and warm sky",
-    date: "forget"
+    img: DragonWaterSky,
+    description: "Dragon Water",
+    location: "Guanghan China",
+    date: "Aug 15 2025"
   },
   {
     title: "TaiWanese Mixed Noodles",
-    img: "src/assets/TaiwaneseMixedNoodles.jpg",
-    description: "little spicy",
-    date: "forget"
-  }
+    img: TaiwaneseMixedNoodles,
+    description: "Ginza",
+    location: "Guanghan China",
+    date: "Jan 5 2026"
+  },
+  {
+    title: "Grass Water",
+    img: GrassWater,
+    description: "DuckRiver",
+    location: "Guanghan China",
+    date: "Jun 15 2025"
+  },
+  {
+    title: "ICPC",
+    img: ICPC,
+    description: "ICPC Coffee tips",
+    location: "LeShan China",
+    date: "Jun 7 2025"
+  },
+  {
+    title: "Wu Hou House",
+    img: WuHouHouse,
+    description: "Wu Hou House",
+    location: "Chengdu China",
+    date: "Aug 27 2025"
+  },
+  {
+    title: "School Sky",
+    img: SchoolSky,
+    description: "SiChuanHangTian",
+    location: "Guanghan China",
+    date: "Jun 18 2025"
+  },
+  {
+    title: "Guang Han Sky",
+    img: GuangHanSky,
+    description: "Guang Han Sky",
+    location: "Guanghan China",
+    date: "Jun 13 2025"
+  },
 ];
